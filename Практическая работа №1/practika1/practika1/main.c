1b7ebb261e75dbd15f147a087fe6f5a7bff5d923
Andrey3ykin <98645157+Andrey3ykin@users.noreply.github.com>
2022-05-12 14:21:18 +0300

    Add files via upload

---
+++ Практическая работа №1/practika1/practika1/main.c
@@ -0,0 +1,20 @@
+#include <8051.h> 
+void main()
+{
+int i;
+char xdata *ptr;
+char test, nabor;
+nabor = 0xAA;
+ptr = (char xdata *) 0x800;
+for(i=0; i<1024;i++)
+
+{
+*ptr=nabor;
+test=*ptr; 
+if(test!=nabor)
+{
+break; 
+}
+ptr++;
+}
+}
